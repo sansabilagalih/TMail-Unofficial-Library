48592b7f6e689fdc2aa5be7eb703c658e07c1d4e
J <40630518+JReverse@users.noreply.github.com>
2021-10-20 23:11:04 -0500

    .Net 6

---
+++ Net 6/TMail Unofficial Library/REST/api/email.cs
@@ -0,0 +1,18 @@
+﻿namespace TMail_Unofficial_Library
+{
+    public static class email
+    {
+        public static async Task<string> GenerateEmail(this TMailClient client)
+        {
+            try
+            {
+                HttpResponseMessage result = await client.HttpClient.Send("email/null");
+                return await result.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Net 6/TMail Unofficial Library/REST/api/messages.cs
@@ -0,0 +1,25 @@
+﻿namespace TMail_Unofficial_Library
+{
+    public static class messages
+    {
+        public static async Task<string> CheckEmail(this TMailClient client, string email)
+        {
+            try
+            {
+                HttpResponseMessage result = await client.HttpClient.Send("messages/" + email);
+                if (await result.Content.ReadAsStringAsync() == "[]")
+                {
+                    return "Waiting.";
+                }
+                else
+                {
+                    return await result.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
