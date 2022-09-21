be3d832b6f9f6741e838c29cbf38b02223884de9
J <40630518+JReverse@users.noreply.github.com>
2022-09-20 19:26:51 -0500

    Add Email length as null no longer works

--- Net 6/TMail Unofficial Library/REST/api/email.cs
+++ Net 6/TMail Unofficial Library/REST/api/email.cs
@@ -2,11 +2,18 @@
 {
     public static class email
     {
-        public static async Task<string> GenerateEmail(this TMailClient client)
+        internal static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static async Task<string> GenerateEmail(this TMailClient client, int email_length)
         {
             try
             {
-                HttpResponseMessage result = await client.HttpClient.Send("email/null");
+                HttpResponseMessage result = await client.HttpClient.Send("email/"+ RandomString(email_length));
                 return await result.Content.ReadAsStringAsync();
             }
             catch (Exception ex)
